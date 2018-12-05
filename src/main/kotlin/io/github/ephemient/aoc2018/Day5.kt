55c4cf1b4751fce10503ccdb0d4fdb1990135521
Daniel Lin <ephemient@gmail.com>
2018-12-05 06:05:42 +0000

    Day 5: Speed up by avoiding intermediate String states

--- src/main/kotlin/io/github/ephemient/aoc2018/Day5.kt
+++ src/main/kotlin/io/github/ephemient/aoc2018/Day5.kt
@@ -3,25 +3,27 @@ package io.github.ephemient.aoc2018
 class Day5(lines: List<String>) {
     private val input = lines.first()
 
-    fun part1(): Int = input.react().length
+    fun part1(): Int = input.react()
 
     fun part2(): Int? = ('a'..'z')
-        .map { input.replace("$it", "", ignoreCase = true).react().length }
+        .map { input.replace("$it", "", ignoreCase = true).react() }
         .min()
 
     companion object {
-        private val PATTERN = ('a'..'z')
-            .map { a ->
-                val b = a.toUpperCase()
-                "$a$b|$b$a"
+        private fun String.react(): Int {
+            if (isEmpty()) return 0
+            val stack = StringBuilder()
+            for ((i, b) in withIndex()) {
+                val a = stack.lastOrNull()
+                if (a != null &&
+                    a.isUpperCase() != b.isUpperCase() &&
+                    a.toUpperCase() == b.toUpperCase()) {
+                    stack.deleteCharAt(stack.lastIndex)
+                } else {
+                    stack.append(b)
+                }
             }
-            .joinToString(separator = "|")
-            .toRegex()
-
-        fun String.react(): String =
-            generateSequence(this) { it.replace(PATTERN, "") }
-                .zipWithNext()
-                .mapNotNull { (a, b) -> a.takeIf { it == b } }
-                .first()
+            return stack.length
+        }
     }
 }
