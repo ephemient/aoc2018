3e9a22702bb7a04ccff93827664b5143c3d1dcf3
Daniel Lin <ephemient@gmail.com>
2018-12-05 20:53:03 +0000

    Day 5: Pre-react before filtering

--- src/Day5.hs
+++ src/Day5.hs
@@ -5,15 +5,16 @@ Description:    <https://adventofcode.com/2018/day/5 Day 5: Alchemical Reaction>
 {-# LANGUAGE ViewPatterns #-}
 module Day5 (day5a, day5b) where
 
-import Data.Char (isAlpha, isUpper, toUpper)
+import Data.Char (isAlpha, toUpper)
 
-react :: String -> Int
-react = length . foldr acc ""
-  where acc a (b:bs) | isUpper a /= isUpper b && toUpper a == toUpper b = bs
+react :: String -> String
+react = foldr acc ""
+  where acc a (b:bs) | a /= b && toUpper a == toUpper b = bs
         acc a bs = a:bs
 
 day5a :: String -> Int
-day5a = react . filter isAlpha
+day5a = length . react . filter isAlpha
 
 day5b :: String -> Int
-day5b (filter isAlpha -> input) = minimum [react $ filter ((/= c) . toUpper) input | c <- ['A'..'Z']]
+day5b (react . filter isAlpha -> input) = minimum
+    [length $ react $ filter ((/= c) . toUpper) input | c <- ['A'..'Z']]
