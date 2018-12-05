a169080248e69444768e784446b9bbd52a3bf24f
Daniel Lin <ephemient@gmail.com>
2018-12-05 06:51:44 +0000

    Revert "Day 5: Speed up by avoiding intermediate String states"
    This reverts commit 55c4cf1b4751fce10503ccdb0d4fdb1990135521.
    
    It's not actually faster, I don't know why.

--- src/main/kotlin/io/github/ephemient/aoc2018/Day5.kt
+++ src/main/kotlin/io/github/ephemient/aoc2018/Day5.kt
@@ -3,27 +3,25 @@ package io.github.ephemient.aoc2018
 class Day5(lines: List<String>) {
     private val input = lines.first()
 
-    fun part1(): Int = input.react()
+    fun part1(): Int = input.react().length
 
     fun part2(): Int? = ('a'..'z')
-        .map { input.replace("$it", "", ignoreCase = true).react() }
+        .map { input.replace("$it", "", ignoreCase = true).react().length }
         .min()
 
     companion object {
-        private fun String.react(): Int {
-            if (isEmpty()) return 0
-            val stack = StringBuilder()
-            for ((i, b) in withIndex()) {
-                val a = stack.lastOrNull()
-                if (a != null &&
-                    a.isUpperCase() != b.isUpperCase() &&
-                    a.toUpperCase() == b.toUpperCase()) {
-                    stack.deleteCharAt(stack.lastIndex)
-                } else {
-                    stack.append(b)
-                }
+        private val PATTERN = ('a'..'z')
+            .map { a ->
+                val b = a.toUpperCase()
+                "$a$b|$b$a"
             }
-            return stack.length
-        }
+            .joinToString(separator = "|")
+            .toRegex()
+
+        private fun String.react(): String =
+            generateSequence(this) { it.replace(PATTERN, "") }
+                .zipWithNext()
+                .mapNotNull { (a, b) -> a.takeIf { it == b } }
+                .first()
     }
 }
