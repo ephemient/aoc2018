6614392a9e02c0def9dfcbac9018d6ca2026a3e0
Daniel Lin <ephemient@gmail.com>
2018-12-05 08:50:33 +0000

    Day 5: Actually fast now (partly thanks to clean build?)

--- src/main/kotlin/io/github/ephemient/aoc2018/Day5.kt
+++ src/main/kotlin/io/github/ephemient/aoc2018/Day5.kt
@@ -3,25 +3,22 @@ package io.github.ephemient.aoc2018
 class Day5(lines: List<String>) {
     private val input = lines.first()
 
-    fun part1(): Int = input.react().length
+    fun part1(): Int = react()
 
-    fun part2(): Int? = ('a'..'z')
-        .map { input.replace("$it", "", ignoreCase = true).react().length }
-        .min()
+    fun part2(): Int? = ('A'..'Z').map { react(it) }.min()
 
-    companion object {
-        private val PATTERN = ('a'..'z')
-            .map { a ->
-                val b = a.toUpperCase()
-                "$a$b|$b$a"
+    private val buffer = CharArray(input.length)
+
+    private fun react(exclude: Char? = null): Int {
+        var pos = 0
+        for (c in input) {
+            if (exclude?.equals(c, ignoreCase = true) == true) continue
+            if (pos > 0 && buffer[pos - 1].let { it != c && it.equals(c, ignoreCase = true) }) {
+                pos--
+            } else {
+                buffer[pos++] = c
             }
-            .joinToString(separator = "|")
-            .toRegex()
-
-        private fun String.react(): String =
-            generateSequence(this) { it.replace(PATTERN, "") }
-                .zipWithNext()
-                .mapNotNull { (a, b) -> a.takeIf { it == b } }
-                .first()
+        }
+        return pos
     }
 }
