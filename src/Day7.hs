9f7433245315720da211c186db94b94149c31b4f
Daniel Lin <ephemient@gmail.com>
2018-12-08 22:49:17 +0000

    Day 7: Use unfoldr instead of explicit recursion and fix non-exhaustive pattern match warning

--- src/Day7.hs
+++ src/Day7.hs
@@ -2,13 +2,13 @@
 Module:         Day7
 Description:    <https://adventofcode.com/2018/day/7 Day 7: The Sum of Its Parts>
 -}
+{-# LANGUAGE TupleSections #-}
 module Day7 (day7a, day7b) where
 
-import Control.Monad (ap)
 import Data.Char (ord)
-import Data.List (sort)
+import Data.List (sort, unfoldr)
 import Data.Map (Map)
-import qualified Data.Map as Map (delete, filter, filterWithKey, fromListWith, keys, lookupMin, map, null)
+import qualified Data.Map as Map (assocs, delete, filter, fromListWith, lookupMin, map, null)
 import Data.Set (Set)
 import qualified Data.Set as Set (delete, empty, null, singleton, union)
 
@@ -17,18 +17,15 @@ parse input = Map.fromListWith Set.union $ concat
     [[(line !! 5, Set.empty), (line !! 36, Set.singleton $ line !! 5)] | line <- lines input]
 
 day7a :: String -> String
-day7a = loop . parse
-  where loop deps = case Map.lookupMin $ Map.filter Set.null deps of
-            Just (k, _) -> k : loop (Map.map (Set.delete k) $ Map.delete k deps)
-            _ | Map.null deps -> []
+day7a = unfoldr f . parse
+  where f deps = case Map.lookupMin $ Map.filter Set.null deps of
+            Just (k, _) -> Just (k, Map.map (Set.delete k) $ Map.delete k deps)
+            _ -> if Map.null deps then Nothing else error "unsatisfied dependencies"
 
 day7b :: Int -> Int -> String -> Int
-day7b cost workers = ap loop (map (start 0) . take workers . Map.keys . Map.filter Set.null) . parse
-  where start t c = (t + cost + ord c - ord 'A' + 1, c)
-        loop deps ((t, k):ready)
-          | Map.null deps' && null ready' = t
-          | otherwise = loop deps' ready'
-          where deps' = Map.map (Set.delete k) $ Map.delete k deps
-                ok k a = k `notElem` map snd ready && Set.null a
-                pending = map (start t) $ Map.keys $ Map.filterWithKey ok deps'
-                ready' = sort $ ready ++ take (workers - length ready) pending
+day7b baseCost workers = last . unfoldr f . ([],) . parse
+  where cost c = baseCost + ord c - ord 'A' + 1
+        f ((t, k):ready, deps) = Just (t, g t ready $ Map.map (Set.delete k) $ Map.delete k deps)
+        f ([], deps) = if Map.null deps then Nothing else Just (0, g 0 [] deps)
+        g t ready deps = (, deps) $ sort $ ready ++ take (workers - length ready)
+            [(t + cost k, k) | (k, a) <- Map.assocs deps, k `notElem` map snd ready, Set.null a]
