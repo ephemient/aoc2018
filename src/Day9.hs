775a889d77c1d44d9d677652bc7ba6a3502f24e7
Daniel Lin <ephemient@gmail.com>
2018-12-09 22:09:34 +0000

    Day 9: Simplify code and reduce memory usage with a flat STUArray instead of Zipper [] []

--- src/Day9.hs
+++ src/Day9.hs
@@ -2,56 +2,38 @@
 Module:         Day9
 Description:    <https://adventofcode.com/2018/day/9 Day 9: Marble Mania>
 -}
-{-# LANGUAGE FlexibleContexts, NamedFieldPuns, PatternGuards, RecordWildCards, StrictData, TypeApplications, ViewPatterns #-}
+{-# LANGUAGE FlexibleContexts, TypeApplications #-}
 module Day9 (day9a, day9b, play) where
 
 import Control.Arrow (second)
-import Data.IntMap (IntMap, elems, empty, insertWith)
-import Data.List (foldl')
+import Control.Monad (foldM, forM_)
+import Control.Monad.ST (ST, runST)
+import Data.Array.ST (STUArray, newArray, readArray, writeArray)
+import Data.IntMap (elems, empty, insertWith)
 import Text.Megaparsec (MonadParsec, parseMaybe, skipManyTill, takeRest)
 import Text.Megaparsec.Char (satisfy)
 import Text.Megaparsec.Char.Lexer (decimal)
 
-data Zipper a = Zipper {left :: [a], here :: a, right :: [a]}
-
-data Game a = Game {scores :: IntMap a, ring :: Zipper a}
-
 parser :: (MonadParsec e String m, Integral players, Integral target) => m (players, target)
 parser = (,) <$> decimal <*> skipManyTill (satisfy $ const True) decimal <* takeRest
 
-singleton :: a -> Zipper a
-singleton here = Zipper {left = [], here, right = []}
-
-moveZipper :: Int -> Zipper a -> Zipper a
-moveZipper n zipper@Zipper {..} = case compare n 0 of
-    EQ -> zipper
-    _  | [] <- left, [] <- right -> zipper
-    LT | (reverse -> a:as, bs) <- splitAt (-n) left ->
-         moveZipper (n + 1 + length as) zipper {left = bs, here = a, right = as ++ (here:right)}
-       | otherwise -> moveZipper n zipper {left = reverse right, right = []}
-    GT | (reverse -> a:as, bs) <- splitAt n right ->
-         moveZipper (n - 1 - length as) zipper {left = as ++ (here:left), here = a, right = bs}
-       | otherwise -> moveZipper n zipper {left = [], right = reverse left}
-
-insertZipper :: a -> Zipper a -> Zipper a
-insertZipper a zipper@Zipper {..} = zipper {left = here:left, here = a}
-
-removeZipper :: Zipper a -> (a, Zipper a)
-removeZipper Zipper {left = [], right = []} = error "empty"
-removeZipper zipper@Zipper {..} = case right of
-    a:as -> (here, zipper {here = a, right = as})
-    [] -> removeZipper zipper {left = [], right = reverse left}
-
 play :: Int -> Int -> Int
-play players target = maximum . elems . scores $
-    foldl' acc Game {scores = empty, ring = singleton 0} [1..target]
-  where acc game@Game {..} n
-          | n `mod` 23 == 0
-          = let scores' = insertWith (+) (n `mod` players) (n + m) scores
-                (m, ring') = removeZipper $ moveZipper (-7) ring
-            in game {scores = scores', ring = ring'}
-          | otherwise
-          = game {ring = insertZipper n $ moveZipper 1 ring}
+play players target = maximum . elems $ runST $ do
+    a <- newArray (0, target - 1) 0 :: ST s (STUArray s Int Int)
+    let ix pos i = (pos + i) `mod` target
+        acc (pos, size, scores) n
+          | n `mod` 23 == 0 = do
+                forM_ [1..6] $ \i -> readArray a (ix pos $ size - i) >>= writeArray a (ix pos (-i))
+                m <- readArray a $ ix pos $ size - 7
+                let scores' = insertWith (+) (n `mod` players) (n + m) scores
+                return (ix pos (-6), size - 1, scores')
+          | otherwise = do
+                readArray a pos >>= writeArray a (ix pos size)
+                readArray a (ix pos 1) >>= writeArray a (ix pos $ size + 1)
+                writeArray a (ix pos 1) n
+                return (ix pos 1, size + 1, scores)
+    (_, _, scores) <- foldM acc (0, 1, empty) [1..target]
+    return scores
 
 day9a :: String -> Maybe Int
 day9a = fmap (uncurry play) . parseMaybe @() parser
