f2145ef13e74e14b31fa1ba04e25d5a6e971921c
Daniel Lin <ephemient@gmail.com>
2018-12-11 14:56:35 -0500

    Day 11: Summed-area table

--- src/main/kotlin/io/github/ephemient/aoc2018/Day11.kt
+++ src/main/kotlin/io/github/ephemient/aoc2018/Day11.kt
@@ -1,39 +1,34 @@
 package io.github.ephemient.aoc2018
 
-class Day11(lines: List<String>) {
-    private val serial = lines.first().toInt()
+class Day11(lines: List<String>, private val size: Int = 300) {
+    private val table = Array(size + 1) { IntArray(size + 1) }.also { table ->
+        val serial = lines.first().toInt()
+        for ((y, x) in IntPair(1, 1)..IntPair(size, size)) {
+            table[y][x] = table[y - 1][x] + table[y][x - 1] - table[y - 1][x - 1] +
+                ((x + 10) * y + serial) * (x + 10) / 100 % 10 - 5
+        }
+    }
 
-    private fun f(x: Int, y: Int): Int = ((x + 11) * (y + 1) + serial) * (x + 11) / 100 % 10 - 5
-
-    fun part1(size: Int = 300): String {
-        require(size > 0)
-        val a = Array(size) { y -> IntArray(size) { x -> f(x, y) } }
+    fun part1(): String {
         val (y, x) = (IntPair(0, 0)..IntPair(size - 3, size - 3)).maxBy { (y, x) ->
-            (IntPair(y, x)..IntPair(y + 2, x + 2)).sumBy { (y, x) -> a[y][x] }
+            table[y + 3][x + 3] - table[y][x + 3] - table[y + 3][x] + table[y][x]
         }!!
         return "${x + 1},${y + 1}"
     }
 
-    fun part2(size: Int = 300): String {
-        require(size > 0)
-        val a = Array(size) { y -> IntArray(size) { x -> f(x, y) } }
-        var best = IntPair(-1, -1)
-        var bestN = -1
-        var bestP = Int.MIN_VALUE
-        val memo = Array(size) { IntArray(size) }
-        for (n in 0 until size) {
-            for (p in IntPair(0, 0)..IntPair(size - n - 1, size - n - 1)) {
-                val (y, x) = p
-                memo[y][x] +=
-                    (y..y + n - 1).sumBy { a[it][x + n] } + (x..x + n).sumBy { a[y + n][it] }
-                if (memo[y][x] > bestP) {
-                    best = p
-                    bestN = n
-                    bestP = memo[y][x]
+    fun part2(): String {
+        var bestPoint: Triple<Int, Int, Int>? = null
+        var maxValue: Int? = null
+        for ((y, x) in IntPair(0, 0)..IntPair(size - 1, size - 1)) {
+            for (n in 1 until size - maxOf(x, y)) {
+                val value = table[y + n][x + n] - table[y][x + n] - table[y + n][x] + table[y][x]
+                if (maxValue == null || maxValue < value) {
+                    bestPoint = Triple(x, y, n)
+                    maxValue = value
                 }
             }
         }
-        check(best.first >= 0 && best.second >= 0 && bestN >= 0)
-        return "${best.second + 1},${best.first + 1},${bestN + 1}"
+        val (x, y, n) = bestPoint!!
+        return "${x + 1},${y + 1},$n"
     }
 }
