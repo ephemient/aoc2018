8627034629836a0ecfd9963e3513343a43c95574
Daniel Lin <ephemient@gmail.com>
2018-12-20 17:57:52 -0500

    Day 20: Fix for cases not covered by input, where (..|..).. multiple tails after alternatives must all be followed
    Surprisingly, this is also faster.

--- src/main/kotlin/io/github/ephemient/aoc2018/Day20.kt
+++ src/main/kotlin/io/github/ephemient/aoc2018/Day20.kt
@@ -3,41 +3,63 @@ package io.github.ephemient.aoc2018
 import java.util.ArrayDeque
 
 class Day20(lines: List<String>) {
-    private val edges: Map<IntPair, Set<IntPair>>
-
-    init {
-        val edges = mutableMapOf<IntPair, MutableSet<IntPair>>()
-        var stack = mutableListOf<IntPair>()
-        var p = IntPair(0, 0)
-        loop@for (c in lines.single()) {
-            val q = when (c) {
-                'N' -> IntPair(p.first, p.second - 1)
-                'E' -> IntPair(p.first + 1, p.second)
-                'S' -> IntPair(p.first, p.second + 1)
-                'W' -> IntPair(p.first - 1, p.second)
-                '(' -> { stack.add(p); continue@loop }
-                '|' -> { p = stack.last(); continue@loop }
-                ')' -> { p = stack.removeAt(stack.lastIndex); continue@loop }
-                else -> continue@loop
+    private val edges: Map<IntPair, Set<IntPair>> =
+        mutableMapOf<IntPair, MutableSet<IntPair>>().apply {
+            val closeStack = mutableListOf<MutableSet<IntPair>>()
+            val openStack = mutableListOf<MutableSet<IntPair>>()
+            var current = mutableSetOf(IntPair(0, 0))
+            loop@for (c in lines.single().removePrefix("^").removeSuffix("$")) {
+                val (dx, dy) = when (c) {
+                    'N' -> north
+                    'E' -> east
+                    'S' -> south
+                    'W' -> west
+                    '(' -> {
+                        closeStack.add(mutableSetOf())
+                        openStack.add(current)
+                        continue@loop
+                    }
+                    '|' -> {
+                        closeStack.last().addAll(current)
+                        current = openStack.last()
+                        continue@loop
+                    }
+                    ')' -> {
+                        closeStack.last().addAll(current)
+                        current = closeStack.removeAt(closeStack.lastIndex)
+                        openStack.removeAt(openStack.lastIndex)
+                        continue@loop
+                    }
+                    else -> error("$c")
+                }
+                current = current.mapTo(mutableSetOf<IntPair>()) { p ->
+                    val q = IntPair(p.first + dx, p.second + dy)
+                    getOrPut(p) { mutableSetOf() }.add(q)
+                    getOrPut(q) { mutableSetOf() }.add(p)
+                    return@mapTo q
+                }
             }
-            edges.getOrPut(p) { mutableSetOf() }.add(q)
-            edges.getOrPut(q) { mutableSetOf() }.add(p)
-            p = q
+            require(closeStack.isEmpty() && openStack.isEmpty())
         }
-        require(stack.isEmpty())
-        this.edges = edges
-    }
 
-    fun part1(): Int = bfs(IntPair(0, 0)).last().first
+    fun part1(): Int = bfs(IntPair(0, 0)).last()
 
-    fun part2(): Int = bfs(IntPair(0, 0)).count { (depth, _) -> depth >= 1000 }
+    fun part2(): Int = bfs(IntPair(0, 0)).dropWhile { it < 1000 }.count()
 
-    private fun bfs(start: IntPair): Sequence<Pair<Int, IntPair>> = sequence<Pair<Int, IntPair>> {
-        val queue = ArrayDeque(listOf(0 to start))
+    private fun bfs(vararg start: IntPair): Sequence<Int> = sequence {
+        val queue = start.mapTo(ArrayDeque()) { 0 to it }
         val seen = mutableSetOf<IntPair>()
         while (queue.isNotEmpty()) {
-            val (depth, p) = queue.remove().also { yield(it) }
+            val (depth, p) = queue.remove()
+            yield(depth)
             edges[p]?.filter(seen::add)?.mapTo(queue) { depth + 1 to it }
         }
+    }
+
+    companion object {
+        private val north = IntPair(0, -1)
+        private val east = IntPair(1, 0)
+        private val south = IntPair(0, 1)
+        private val west = IntPair(-1, 0)
     }
 }
