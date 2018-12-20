643627de58871657e51857deea73bc25dc974656
Daniel Lin <ephemient@gmail.com>
2018-12-20 17:27:47 -0500

    Day 20: Fix for cases not covered by input, where (..|..).. multiple tails after alternatives must all be followed
    Surprisingly, this is also faster.

--- src/Day20.hs
+++ src/Day20.hs
@@ -2,36 +2,53 @@
 Module:         Day20
 Description:    <https://adventofcode.com/2018/day/20 Day 20: A Regular Map>
 -}
-{-# LANGUAGE TypeApplications, ViewPatterns #-}
+{-# LANGUAGE BangPatterns, FlexibleContexts, TupleSections, ViewPatterns #-}
 module Day20 (day20a, day20b) where
 
+import Control.Arrow (first, second)
+import Control.Monad.State.Strict (evalState, foldM, state)
 import Data.Char (isSpace)
-import Data.Graph.Inductive (UGr, level, mkUGraph)
+import Data.IntSet (IntSet, (\\))
+import qualified Data.IntSet as IntSet (empty, singleton, toList, union)
 import Data.List (dropWhileEnd)
-import Data.Map.Strict ((!), elems, insertLookupWithKey, singleton, size)
+import Data.Map.Strict (Map, findWithDefault, insertLookupWithKey, insertWith, size)
+import qualified Data.Map.Strict as Map (empty, singleton)
 import Data.Maybe (fromMaybe)
+import Data.Sequence (Seq((:<|)), (><))
+import qualified Data.Sequence as Seq (fromList, singleton)
+import Data.Set (mapMonotonic)
+import qualified Data.Set as Set (singleton, union)
 
-buildGraph :: String -> UGr
-buildGraph (dropWhile (== '^') . dropWhileEnd (== '$') . filter (not . isSpace) -> input) =
-    mkUGraph (elems vertices) edges
-  where (vertices, edges) = buildGraph' [] (singleton @(Int, Int) (0, 0) 0) [] (0, 0) input
-        buildGraph' stack vs es p ('(':ds) = buildGraph' (p:stack) vs es p ds
-        buildGraph' stack@(p:_) vs es _ ('|':ds) = buildGraph' stack vs es p ds
-        buildGraph' (p:stack) vs es _ (')':ds) = buildGraph' stack vs es p ds
-        buildGraph' stack vs es p@(x, y) (d:ds) = buildGraph' stack vs' ((vs ! p, v):es) p' ds
-          where p' = case d of
-                    'N' -> (x, y - 1)
-                    'E' -> (x + 1, y)
-                    'S' -> (x, y + 1)
-                    'W' -> (x - 1, y)
-                    _ -> error $ "not a valid direction: " ++ [d]
-                (fromMaybe (size vs) -> v, vs') =
-                    insertLookupWithKey (\_ _ a -> a) p' (size vs) vs
-        buildGraph' [] vs es _ "" = (vs, es)
-        buildGraph' _ _ _ _ _ = error "mismatched parentheses"
+buildGraph :: String -> Map Int IntSet
+buildGraph (dropWhile (== '^') . dropWhileEnd (== '$') . filter (not . isSpace) -> input) = graph
+  where start = (0, 0) :: (Int, Int)
+        ([_], graph) = flip evalState (Map.singleton start 0) $
+            foldM acc ([Set.singleton start], Map.empty) input
+        toKey p = state $ \m@(size -> n) ->
+            first (fromMaybe n) $ insertLookupWithKey (\_ _ a -> a) p n m
+        acc (p:ps, !m) 'N' = (mapMonotonic north p : ps,) <$> foldM (move north) m p
+        acc (p:ps, !m) 'E' = (mapMonotonic east p : ps,) <$> foldM (move east) m p
+        acc (p:ps, !m) 'S' = (mapMonotonic south p : ps,) <$> foldM (move south) m p
+        acc (p:ps, !m) 'W' = (mapMonotonic west p : ps,) <$> foldM (move west) m p
+        acc (p:ps, !m) '(' = return (p:p:p:ps, m)
+        acc (p:q:r:ps, !m) '|' = return (q : q : Set.union p r : ps, m)
+        acc (p:_:q:ps, !m) ')' = return (Set.union p q : ps, m)
+        (north, east, south, west) = (second pred, first succ, second succ, first pred)
+        move f m p = do
+            u <- toKey p
+            v <- toKey $ f p
+            return $ insertWith IntSet.union u (IntSet.singleton v) $
+                insertWith IntSet.union v (IntSet.singleton u) m
+
+bfs :: Int -> Map Int IntSet -> [Int]
+bfs start graph = bfs' IntSet.empty $ Seq.singleton (0, start)
+  where bfs' seen ((depth, v) :<| queue) = depth : bfs' (IntSet.union seen next)
+            (queue >< Seq.fromList ((depth + 1,) <$> IntSet.toList next))
+          where next = findWithDefault IntSet.empty v graph \\ seen
+        bfs' _ _ = []
 
 day20a :: String -> Int
-day20a = maximum . fmap snd . level 0 . buildGraph
+day20a = maximum . bfs 0 . buildGraph
 
 day20b :: String -> Int
-day20b = length . filter (>= 1000) . fmap snd . level 0 . buildGraph
+day20b = length . dropWhile (< 1000) . bfs 0 . buildGraph
