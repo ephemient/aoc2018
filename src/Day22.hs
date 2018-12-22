6f5b0cccea35c172a26c1815e0cc54689fb56ab6
Daniel Lin <ephemient@gmail.com>
2018-12-22 19:11:40 +0000

    Day 22: Simplify initial maze computation now that growing past target is handled separately
    Also include ((x, y), e) in the priority heap ordering, in hopes of
    guiding the search towards smaller coordinates. (Nope, it doesn't.)

--- src/Day22.hs
+++ src/Day22.hs
@@ -8,7 +8,7 @@ module Day22 (day22a, day22b) where
 import Control.Monad.ST (runST)
 import Data.Array.Unboxed (IArray, Ix, UArray, (!), (//), accumArray, assocs, bounds, elems, listArray, rangeSize)
 import Data.Function (on)
-import qualified Data.Heap as Heap (FstMinPolicy, insert, singleton, view)
+import qualified Data.Heap as Heap (MinPolicy, insert, singleton, view)
 import qualified Data.Map.Strict as Map (empty, insert, lookup)
 import Data.STRef (newSTRef, modifySTRef', readSTRef, writeSTRef)
 import Data.List ((\\), scanl')
@@ -24,13 +24,10 @@ erosion, risk :: (Integral a) => a -> a -> a
 erosion depth = (`mod` 20183) . (+ depth)
 risk depth = (`mod` 3) . erosion depth
 
-makeMaze :: (IArray a e, Enum e, Integral e, Ix e) => e -> (e, e) -> (e, e) -> a (e, e) e
-makeMaze depth (targetX, targetY) size@(_, sizeY) =
-    listArray ((0, 0), size) $ scanl acc [0, 48271..] [1..] >>= take (rangeSize (0, sizeY))
-  where acc (drop 1 -> prev) x = scanl' (acc' x) (16807 * x) $ zip [1..] prev
-        acc' x left (y, up)
-          | x == targetX && y == targetY = 0
-          | otherwise = erosion depth left * erosion depth up
+makeMaze :: (IArray a e, Enum e, Integral e, Ix e) => e -> (e, e) -> a (e, e) e
+makeMaze depth target@(_, targetY) = listArray ((0, 0), target)
+    (scanl acc [0, 48271..] [1..] >>= take (rangeSize (0, targetY))) // [(target, 0)]
+  where acc prev x = scanl' ((*) `on` erosion depth) (16807 * x) $ drop 1 prev
 
 growMaze :: (IArray a e, Enum e, Integral e, Ix e) => e -> (e, e) -> a (e, e) e -> a (e, e) e
 growMaze depth (sizeX, sizeY) prior = top // bottom
@@ -47,22 +44,22 @@ growMaze depth (sizeX, sizeY) prior = top // bottom
 day22a :: String -> Maybe Int
 day22a input = do
     (depth, target) <- parseMaybe @() (parser @Int @Int) input
-    return $ sum $ risk depth <$> elems (makeMaze @UArray depth target target)
+    return $ sum $ risk depth <$> elems (makeMaze @UArray depth target)
 
 day22b :: String -> Maybe Int
 day22b input = do
     (depth, target) <- parseMaybe @() (parser @Int @Int) input
     runST $ do
-        mazeRef <- newSTRef $ makeMaze @UArray depth target target
+        mazeRef <- newSTRef $ makeMaze @UArray depth target
         wsRef <- newSTRef Map.empty
-        let bfs (Heap.view -> Just ((_, (w, k@(e, p))), heap))
+        let bfs (Heap.view -> Just ((_, (w, k@(p, e))), heap))
               | 1 <- e, (0, 0) <- p = return $ Just w
               | otherwise = Map.lookup k <$> readSTRef wsRef >>= \case
                     Just w' | w' <= w -> bfs heap
                     _ -> modifySTRef' wsRef (Map.insert k w) >>
-                        foldr (Heap.insert . estimate) heap <$> neighbors w e p >>= bfs
+                        foldr (Heap.insert . estimate) heap <$> neighbors w p e >>= bfs
             bfs _ = return Nothing
-            neighbors w e p@(x, y) = do
+            neighbors w p@(x, y) e = do
                 maze <- readSTRef mazeRef
                 let ((0, 0), (maxX, maxY)) = bounds maze
                 maze' <- if x < maxX && y < maxY then return maze else do
@@ -70,11 +67,11 @@ day22b input = do
                     maze' <$ writeSTRef mazeRef maze'
                 ws <- readSTRef wsRef
                 return $ filter (\(w', k) -> maybe True (> w') $ Map.lookup k ws) $
-                    [(w + 7, (e', p)) | e' <- [0..2] \\ [risk depth $ maze' ! p, e]] ++
-                    [(w + 1, (e, p')) | x > 0, let p' = (x - 1, y), risk depth (maze' ! p') /= e] ++
-                    [(w + 1, (e, p')) | y > 0, let p' = (x, y - 1), risk depth (maze' ! p') /= e] ++
-                    [(w + 1, (e, p')) | let p' = (x + 1, y), risk depth (maze' ! p') /= e] ++
-                    [(w + 1, (e, p')) | let p' = (x, y + 1), risk depth (maze' ! p') /= e]
-        bfs $ Heap.singleton @Heap.FstMinPolicy (0, (0, (1, target)))
-  where estimate a@(w, (e, (x, y))) = (w + x + y + if e == 1 then 0 else 7, a)
+                    [(w + 7, (p, e')) | e' <- [0..2] \\ [risk depth $ maze' ! p, e]] ++
+                    [(w + 1, (p', e)) | x > 0, let p' = (x - 1, y), risk depth (maze' ! p') /= e] ++
+                    [(w + 1, (p', e)) | y > 0, let p' = (x, y - 1), risk depth (maze' ! p') /= e] ++
+                    [(w + 1, (p', e)) | let p' = (x + 1, y), risk depth (maze' ! p') /= e] ++
+                    [(w + 1, (p', e)) | let p' = (x, y + 1), risk depth (maze' ! p') /= e]
+        bfs $ Heap.singleton @Heap.MinPolicy (0, (0, (target, 1)))
+  where estimate a@(w, ((x, y), e)) = (w + x + y + if e == 1 then 0 else 7, a)
         grow n = head . dropWhile (<= n) . iterate (* 2)
