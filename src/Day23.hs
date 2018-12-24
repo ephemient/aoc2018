3a506197ac0d1223b02b227ef86b27fd1a06bff9
Daniel Lin <ephemient@gmail.com>
2018-12-24 23:08:01 +0000

    Day 23: Correct answer in reasonable runtime

--- src/Day23.hs
+++ src/Day23.hs
@@ -2,19 +2,25 @@
 Module:         Day23
 Description:    <https://adventofcode.com/2018/day/23 Day 23: Experimental Emergency Teleportation>
 -}
-{-# LANGUAGE FlexibleContexts, RecordWildCards, TypeApplications #-}
+{-# LANGUAGE FlexibleContexts, LambdaCase, RecordWildCards, TypeApplications, ViewPatterns #-}
 module Day23 (day23a, day23b) where
 
 import Control.Monad (ap)
-import Data.List (foldl', sortOn)
-import Data.Maybe (listToMaybe)
+import Control.Monad.State.Strict (execState, get, gets, put)
+import Data.Function (on)
+import Data.IntSet (singleton, size, union, unions)
+import Data.List (sortOn)
+import Data.Map.Strict (elems, delete, fromListWith, mapKeysMonotonic, mapKeysWith, maxViewWithKey, partitionWithKey)
+import Data.Maybe (fromJust, listToMaybe)
 import Data.Ord (Down(Down))
-import Debug.Trace (traceShow, traceShowId)
 import Text.Megaparsec (MonadParsec, parseMaybe, sepEndBy)
 import Text.Megaparsec.Char (newline, string, space)
 import Text.Megaparsec.Char.Lexer (decimal, signed)
 
 data Bot a = Bot {x :: a, y :: a, z :: a, r :: a}
+
+data Quad a = Quad {xyz :: a, xy'z :: a, x'y'z :: a, x'yz :: a}
+  deriving (Eq, Ord)
 
 parser :: (Integral a, MonadParsec e String m) => m [Bot a]
 parser = flip sepEndBy newline $ Bot
@@ -28,6 +34,23 @@ maximumsOn f = map fst . foldl maxGroup [] . (zip `ap` map f)
   where maxGroup as@((_, y):_) a@(_, x) = case compare x y of LT -> as; EQ -> a:as; GT -> [a]
         maxGroup _ a = [a]
 
+botToOct :: (Num a) => Bot a -> (Quad a, Quad a)
+botToOct Bot {..} = (Quad (t - r) (u - r) (v - r) (w - r), Quad (t + r) (u + r) (v + r) (w + r))
+  where t = x + y + z; u = x + y - z; v = x - y - z; w = x - y + z
+
+intersect :: (Num a, Ord a) => (Quad a, Quad a) -> (Quad a, Quad a) -> Maybe (Quad a, Quad a)
+intersect (q1, q2) (q3, q4)
+  | p <= t, q <= u, r <= v, s <= w = Just (Quad p q r s, Quad t u v w)
+  | otherwise = Nothing
+  where p = (max `on` xyz) q1 q3; q = (max `on` xy'z) q1 q3
+        r = (max `on` x'y'z) q1 q3; s = (max `on` x'yz) q1 q3
+        t = (min `on` xyz) q2 q4; u = (min `on` xy'z) q2 q4
+        v = (min `on` x'y'z) q2 q4; w = (min `on` x'yz) q2 q4
+
+octToOrigin :: (Num a, Ord a) => (Quad a, Quad a) -> a
+octToOrigin (Quad p q r s, Quad t u v w) =
+    maximum [(min `on` abs) p t, (min `on` abs) q u, (min `on` abs) r v, (min `on` abs) s w]
+
 day23a :: String -> Maybe Int
 day23a input = do
     bots <- parseMaybe @() (parser @Int) input
@@ -39,27 +62,20 @@ day23a input = do
 
 day23b :: String -> Maybe Int
 day23b input = do
-    boxes <- map botToBox <$> parseMaybe @() (parser @Int) input
-    let maxBoxes = map snd $ traceShowId $ maximumsOn fst
-          [ (length overlaps, (lo, foldl' min4 hi overlaps))
-          | (lo, hi) <- boxes
-          , let overlaps = [hi' | (lo', hi') <- boxes, min4 lo lo' == lo', min4 lo hi' == lo]
-          ]
-    return $ minimum $ boxToOrigin <$> traceShowId maxBoxes
-  where botToBox Bot {..} = ((t - r, u - r, v - r, w - r), (t + r, u + r, v + r, w + r))
-          where t = x + y + z; u = x + y - z; v = x - y - z; w = x - y + z
-        min4 (p, q, r, s) (t, u, v, w) = (min p t, min q u, min r v, min s w)
-        boxToOrigin ((p, q, r, s), (t, u, v, w))
-          | traceShow ((x0, y0, z0), (x1, y1, z1)) False = undefined
-          | x0 > 0 && x1 > 0 && y0 > 0 && y1 > 0 && z0 > 0 && z1 > 0 = p
-          | x0 > 0 && x1 > 0 && y0 > 0 && y1 > 0 && z0 < 0 && z1 < 0 = q
-          | x0 > 0 && x1 > 0 && y0 < 0 && y1 < 0 && z0 < 0 && z1 < 0 = r
-          | x0 > 0 && x1 > 0 && y0 < 0 && y1 < 0 && z0 > 0 && z1 > 0 = s
-          | x0 < 0 && x1 < 0 && y0 < 0 && y1 < 0 && z0 < 0 && z1 < 0 = -t
-          | x0 < 0 && x1 < 0 && y0 < 0 && y1 < 0 && z0 > 0 && z1 > 0 = -u
-          | x0 < 0 && x1 < 0 && y0 > 0 && y1 > 0 && z0 > 0 && z1 > 0 = -v
-          | x0 < 0 && x1 < 0 && y0 > 0 && y1 > 0 && z0 < 0 && z1 < 0 = -w
-          | otherwise = error "unimplemented"
-          where (x0, _) = (p + r) `divMod` 2; (x1, _) = (t + v) `divMod` 2
-                (y0, _) = (q - r) `divMod` 2; (y1, _) = (u - v) `divMod` 2
-                (z0, _) = (p - q) `divMod` 2; (z1, _) = (t - u) `divMod` 2
+    bots <- parseMaybe @() (parser @Int) input
+    let octs = fromListWith union $ zip (botToOct <$> bots) (singleton <$> [1..])
+    snd $ execState (go octs) (0, Nothing)
+  where go octs@(size . unions . elems -> remaining) = gets fst >>= \case
+            best | best <= remaining, Just ((oct, n), rest) <- maxViewWithKey octs -> do
+                let (super, sub) = partitionWithKey (\oct' _ -> oct == oct') $
+                        mapKeysMonotonic fromJust $ delete Nothing $
+                        mapKeysWith union (intersect oct) rest
+                    n' = unions $ n : elems super
+                record (size n') $ octToOrigin oct
+                go $ union n' <$> sub
+                go rest
+            _ -> return ()
+        record n m = get >>= \case
+            (best, _) | best < n -> put (n, Just m)
+            (best, m') | best == n -> put (n, max m' $ Just m)
+            _ -> return ()
